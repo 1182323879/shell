a194a8518915029812fc350aaba767aeafe59280
初墨似雪 <1182323879@qq.com>
2025-01-08 16:08:49 +0800

    插电状态

--- dwmbar/battery.sh
+++ dwmbar/battery.sh
@@ -5,29 +5,40 @@ value=$(acpi -b | awk -F , '{ print $2 }' | tr -d "%" )
 
 text=$(acpi -b | awk -F , '{ print $2 }')
 
-state=$(acpi -b | awk -F : '{ print $2 }' | awk -F , '{print $1}' | tr -d " " )
+battery=$(acpi -b | awk -F : '{ print $2 }' | awk -F , '{print $1}' | tr -d " " )
+
+plug=$(acpi -V | grep Adapter | awk '{print $3}' )
+
+case "$plug" in
+	on-line)
+		plug_icon=
+		;;
+	off-line)
+		plug_icon=󱐤
+		;;
+esac
 
 case "$state" in
-	Charging )
+	Charging)
 if [ $value -lt 20 ] ; then
-	icon="󰢟"
+	battery_icon="󰢟"
 elif [ $value -lt 40 ] ; then
-	icon="󱊤"
+	battery_icon="󱊤"
 elif [ $value -lt 80 ] ; then
-	icon="󱊥"
+	battery_icon="󱊥"
 else 
-	icon="󱊦"
+	battery_icon="󱊦"
 fi
 		;;
 	*)
 if [ $value -lt 20 ] ; then
-	icon="󰂎"
+	battery_icon="󰂎"
 elif [ $value -lt 40 ] ; then
-	icon="󱊡"
+	battery_icon="󱊡"
 elif [ $value -lt 80 ] ; then
-	icon="󱊢"
+	battery_icon="󱊢"
 else 
-	icon="󱊣"
+	battery_icon="󱊣"
 fi
 		;;
 esac
@@ -35,6 +46,4 @@ esac
 
 sed -i '/battery/d' ./bar
 
-printf "export battery='%s %s'\n" $icon $text >>bar
-
-echo $icon
+printf "export battery='%s%s %s'\n" $plug_icon $battery_icon $text >>bar
